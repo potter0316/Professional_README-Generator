93b51c4dd47b6d18b6958fcdb9c38c228e27fe49
potter0316 <117317671+potter0316@users.noreply.github.com>
2023-02-12 15:01:13 -0600

    function to write README file

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,7 @@
 // Creaded inquirer package from npm
 const inquirer = require('inquirer');
+const fs = require('fs');
+const { rejects } = require('assert');
 // TODO: Create an array of questions for user input
 const questions =[
     {
@@ -30,7 +32,8 @@ const questions =[
     ]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+fs.writeToFile('newREADME.md', process.argv[2], (err) =>
+    err ? console.error(err) : console.log('success!'));
 
 // TODO: Create a function to initialize app
 function init() {}
