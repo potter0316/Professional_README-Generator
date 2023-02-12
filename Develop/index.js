d1994c206d267911817e49ea4685c84959acd0cb
potter0316 <117317671+potter0316@users.noreply.github.com>
2023-02-12 14:10:32 -0600

    created outline for questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,33 @@
 // Creaded inquirer package from npm
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions =[
+    {
+        type: 'input',
+        message: 'Description',
+        name: 'username',
+      },
+      {
+        type: 'input',
+        message: 'Installation?',
+        name: 'password',
+      },
+      {
+        type: 'input',
+        message: 'Usage',
+        name: 'confirm',
+    },
+    {
+        type: 'input',
+        message: 'Contributing',
+        name: 'username',
+      },
+      {
+        type: 'input',
+        message: 'Tests',
+        name: 'password',
+      },
+    ]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
